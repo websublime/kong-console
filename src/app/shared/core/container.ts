c9b7fefd38835048627fbc6841d7c0c103b8de3e
Miguel Ramos <miguel.ramos@yieldify.com>
2016-08-01 23:53:16 +0100

    Added docblock to define issue work around.

--- src/app/shared/core/container.ts
+++ src/app/shared/core/container.ts
@@ -8,6 +8,14 @@ export abstract class Container extends Descriptor {
     super();
   }
 
+  /**
+   * Work around for issue expression has changed after checked
+   * https://github.com/angular/angular/issues/6005#issuecomment-233547490
+   *
+   * @param {Function} fn
+   * @param {Array<any>} [args]
+   * @param {Object} [context]
+   */
   changeDetection(fn: Function, args?: Array<any>, context?: Object): void {
     setTimeout(
       () => {
