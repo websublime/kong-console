bbd7cd61d3e58cb22c54818ca4407a4fa77440df
Miguel Ramos <miguel.ramos@yieldify.com>
2016-08-29 15:24:35 +0100

    Added console.log for init state.

--- src/app/containers/login/login.container.ts
+++ src/app/containers/login/login.container.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
-import { Container, AuthService } from '../../shared';
+import { Component, OnInit } from '@angular/core';
+import { Container, AuthService, State } from '../../shared';
 
 @Component({
   moduleId: __filename,
   selector: 'login',
   templateUrl: './login.template.html'
 })
-export class Login extends Container {
+export class Login extends Container implements OnInit {
   constructor(
+    private _appState: State,
     private _authService: AuthService
   ) {
     super();
+  }
+
+  ngOnInit() {
+    console.log(this._appState);
   }
 }
