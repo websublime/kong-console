689e235e62238129ba74c80476b118d44748198d
Miguel Ramos <miguel.ramos@yieldify.com>
2016-09-02 22:09:03 +0100

    Initiate consumer pages.

--- src/app/containers/+admin/index.ts
+++ src/app/containers/+admin/index.ts
@@ -3,3 +3,4 @@ export * from './home/home.container';
 export * from './apis/apis.container';
 export * from './apis/new.container';
 export * from './apis/edit.container';
+export * from './consumers/consumers.container';
