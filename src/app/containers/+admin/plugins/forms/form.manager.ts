f46f59295e9f08453699e5eb4b70f637bca55ccb
Miguel Ramos <miguel.ramos@yieldify.com>
2016-09-20 23:24:22 +0100

    Added JWT dynamic plugin form.

--- src/app/containers/+admin/plugins/forms/form.manager.ts
+++ src/app/containers/+admin/plugins/forms/form.manager.ts
@@ -4,7 +4,8 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ControlBase, ControlSignature } from './control.base';
 import {
   BasicModelConfig, BasicModel, KeyModelConfig, KeyModel,
-  OAuthModelConfig, OAuthModel
+  OAuthModelConfig, OAuthModel, HMacModelConfig, HMacModel,
+  JWTModel, JWTModelConfig
 } from '../../../../shared';
 
 export interface FormSettings {
@@ -447,6 +448,277 @@ export const FORM_SETTINGS: DynamicFormSettings = <DynamicFormSettings>{
       'clientId': 'client_id',
       'clientSecret': 'client_secret',
       'redirectUri': 'redirect_uri'
+    }
+  },
+  'hmac-auth-config': {
+    title: 'HMAC Authentication',
+    formModel: HMacModelConfig,
+    controls: [
+      new ControlBase<boolean>(<ControlSignature<boolean>>{
+        type: 'checkbox',
+        value: false,
+        control: new FormControl(false),
+        label: 'Hide Credentials',
+        key: 'hideCredentials',
+        errorMsg: null,
+        required: false
+      }),
+      new ControlBase<number>(<ControlSignature<number>>{
+        type: 'number',
+        value: 300,
+        control: new FormControl(300),
+        label: 'Clock Skew',
+        key: 'clockSkew',
+        errorMsg: null,
+        required: false
+      }),
+      new ControlBase<string>(<ControlSignature<string>>{
+        type: 'text',
+        value: '',
+        control: new FormControl('', Validators.required),
+        label: 'Name',
+        key: 'name',
+        errorMsg: null,
+        required: true,
+        render: false
+      })
+    ],
+    /* tslint:disable */
+    help: `
+    <table class="table table-hover">
+      <tr>
+        <th>Attribute</th>
+        <th>Description</th>
+      </tr>
+      <tr>
+        <td><span class="badge-highlight">hide_credentials</span><br><em>optional</em></td>
+        <td><p>An optional boolean value telling the plugin to hide the credential to the upstream API server. It will be removed by Kong before proxying the request.</p></td>
+      </tr>
+      <tr>
+        <td><span class="badge-highlight">clock_skew</span><br><em>optional</em></td>
+        <td><p>Clock Skew in seconds to prevent replay attacks.</p></td>
+      </tr>
+    </table>
+    `,
+    /* tslint:enable */
+    attributes: {
+      'name': 'name',
+      'hideCredentials': 'config.hide_credentials',
+      'clockSkew': 'config.clock_skew'
+    }
+  },
+  'hmac-auth-consumer': {
+    title: 'HMAC Authentication',
+    formModel: HMacModel,
+    controls: [
+      new ControlBase<string>(<ControlSignature<string>>{
+        type: 'text',
+        value: '',
+        control: new FormControl('', Validators.required),
+        label: 'Username',
+        key: 'username',
+        errorMsg: null,
+        required: true
+      }),
+      new ControlBase<string>(<ControlSignature<string>>{
+        type: 'text',
+        value: '',
+        control: new FormControl(''),
+        label: 'Secret',
+        key: 'secret',
+        errorMsg: null,
+        required: false
+      })
+    ],
+    help: `
+    <table class="table table-hover">
+      <tr>
+        <th>Attribute</th>
+        <th>Description</th>
+      </tr>
+      <tr>
+        <td><span class="badge-highlight">username</span></td>
+        <td><p>The username to use in the HMAC Signature verification.</p></td>
+      </tr>
+      <tr>
+        <td><span class="badge-highlight">secret</span><br><em>optional</em></td>
+        <td><p>The secret to use in the HMAC Signature verification.</p></td>
+      </tr>
+    </table>
+    `,
+    attributes: {
+      'username': 'username',
+      'secret': 'secret'
+    }
+  },
+  'jwt-config': {
+    title: 'JSON Web Tokens',
+    formModel: JWTModelConfig,
+    controls: [
+      new ControlBase<string>(<ControlSignature<string>>{
+        type: 'text',
+        value: '',
+        control: new FormControl('', Validators.required),
+        label: 'name',
+        key: 'name',
+        errorMsg: null,
+        required: true,
+        render: false
+      }),
+      new ControlBase<Array<string>>(<ControlSignature<Array<string>>>{
+        type: 'text',
+        value: [],
+        control: new FormControl(''),
+        label: 'URI Parameters Names',
+        key: 'uriParamNames',
+        errorMsg: null,
+        required: false
+      }),
+      new ControlBase<Array<string>>(<ControlSignature<Array<string>>>{
+        type: 'text',
+        value: [],
+        control: new FormControl(''),
+        label: 'Claims to Verify',
+        key: 'claimsToVerify',
+        errorMsg: null,
+        required: false
+      }),
+      new ControlBase<string>(<ControlSignature<string>>{
+        type: 'text',
+        value: '',
+        control: new FormControl(''),
+        label: 'Key Claim Name',
+        key: 'keyClaimName',
+        errorMsg: null,
+        required: false
+      }),
+      new ControlBase<boolean>(<ControlSignature<boolean>>{
+        type: 'checkbox',
+        value: false,
+        control: new FormControl(false),
+        label: 'Secret is Base64',
+        key: 'secretIsBase64',
+        errorMsg: null,
+        required: false
+      })
+    ],
+    /* tslint:disable */
+    help: `
+    <table class="table table-hover">
+      <tr>
+        <th>Attribute</th>
+        <th>Description</th>
+      </tr>
+      <tr>
+        <td><span class="badge-highlight">uri_param_names</span><br><em>optional</em></td>
+        <td><p>A list of querystring parameters that Kong will inspect to retrieve JWTs.</p></td>
+      </tr>
+      <tr>
+        <td><span class="badge-highlight">claims_to_verify</span><br><em>optional</em></td>
+        <td><p>A list of registered claims (according to <a href="https://tools.ietf.org/html/rfc7519">RFC 7519</a>) that Kong can verify as well. Accepted values: <span class="badge-highlight">exp</span>, <span class="badge-highlight">nbf</span>.</p></td>
+      </tr>
+      <tr>
+        <td><span class="badge-highlight">key_claim_name</span><br><em>optional</em></td>
+        <td><p>The name of the claim in which the <span class="badge-highlight">key</span> identifying the secret must be passed.</p></td>
+      </tr>
+      <tr>
+        <td><span class="badge-highlight">secret_is_base64</span><br><em>optional</em></td>
+        <td><p>If true, the plugin assumes the credential's <span class="badge-highlight">secret</span> to be base64 encoded. You will need to create a base64 encoded secret for your consumer, and sign your JWT with the original secret.</p></td>
+      </tr>
+    </table>
+    `,
+    /* tslint:enable */
+    attributes: {
+      'name': 'name',
+      'uriParamNames': 'config.uri_param_names',
+      'claimsToVerify': 'config.claims_to_verify',
+      'keyClaimName': 'config.key_claim_name',
+      'secretIsBase64': 'config.secret_is_base64'
+    },
+    beforeUpdateModel: (form: FormGroup) => {
+      let uriParamNames = form.value.uriParamNames
+        ? form.value.uriParamNames.split(',').map((value: string) => { return value.trim(); })
+        : [];
+      let claimsToVerify = form.value.claimsToVerify
+        ? form.value.claimsToVerify.split(',').map((value: string) => { return value.trim(); })
+        : [];
+
+      form.get('uriParamNames').setValue(uriParamNames);
+      form.get('claimsToVerify').setValue(claimsToVerify);
+    }
+  },
+  'jwt-consumer': {
+    title: 'JSON Web Tokens',
+    formModel: JWTModel,
+    controls: [
+      new ControlBase<string>(<ControlSignature<string>>{
+        type: 'text',
+        value: '',
+        control: new FormControl(''),
+        label: 'Key',
+        key: 'key',
+        errorMsg: null,
+        required: false
+      }),
+      new ControlBase<string>(<ControlSignature<string>>{
+        type: 'text',
+        value: 'HS256',
+        control: new FormControl('HS256'),
+        label: 'Algorithm',
+        key: 'algorithm',
+        errorMsg: null,
+        required: false
+      }),
+      new ControlBase<string>(<ControlSignature<string>>{
+        type: 'textarea',
+        value: '',
+        control: new FormControl(''),
+        label: 'RSA Public Key',
+        key: 'rsaPublicKey',
+        errorMsg: null,
+        required: false
+      }),
+      new ControlBase<string>(<ControlSignature<string>>{
+        type: 'text',
+        value: '',
+        control: new FormControl(''),
+        label: 'Secret',
+        key: 'secret',
+        errorMsg: null,
+        required: false
+      })
+    ],
+    /* tslint:disable */
+    help: `
+    <table class="table table-hover">
+      <tr>
+        <th>Attribute</th>
+        <th>Description</th>
+      </tr>
+      <tr>
+        <td><span class="badge-highlight">key</span><br><em>optional</em></td>
+        <td><p>A unique string identifying the credential. If left out, it will be auto-generated. However, usage of this key is mandatory while crafting your token, as specified in the next section.</p></td>
+      </tr>
+      <tr>
+        <td><span class="badge-highlight">algorithm</span><br><em>optional</em></td>
+        <td><p>The algorithm used to verify the token's signature. Can be <span class="badge-highlight">HS256</span> or <span class="badge-highlight">RS256</span>.</p></td>
+      </tr>
+      <tr>
+        <td><span class="badge-highlight">rsa_public_key</span><br><em>optional</em></td>
+        <td><p>If <span class="badge-highlight">algorithm</span> is <span class="badge-highlight">RS256</span>, the public key (in PEM format) to use to verify the token's signature.</p></td>
+      </tr>
+      <tr>
+        <td><span class="badge-highlight">secret</span><br><em>optional</em></td>
+        <td><p>If <span class="badge-highlight">algorithm</span> is <span class="badge-highlight">HS256</span>, the secret used to sign JWTs for this credential. If left out, will be auto-generated. If <span class="badge-highlight">algorithm</span> is <span class="badge-highlight">RS256</span>, this is the private key (in PEM format) to use to verify the token's signature.</p></td>
+      </tr>
+    </table>
+    `,
+    /* tslint:enable */
+    attributes: {
+      'key': 'key',
+      'algorithm': 'algorithm',
+      'rsaPublicKey': 'rsa_public_key',
+      'secret': 'secret'
     }
   }
 };
